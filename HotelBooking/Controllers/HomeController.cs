e0025ace1e56a0c0a19edd02fba2e4919d83842a
RAMRAM66 <ram-asou@mail.ru>
2020-11-08 18:10:02 +0300

    Лаб. раб. 1 выполнена

--- HotelBooking/Controllers/HomeController.cs
+++ HotelBooking/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using HotelBooking.Models;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebGrease.Activities;
 
 namespace HotelBooking.Controllers
 {
@@ -15,6 +17,33 @@ namespace HotelBooking.Controllers
             IEnumerable<Room> rooms = db.Rooms;
             ViewBag.Rooms = rooms;
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.RoomId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            order.Date = DateTime.Now;
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Спасибо, " + order.Person + ", за бронирование!";
+        }
+        public string Sum(double a, double b)
+        {
+            double s = a * b;
+            return $"<h2>Сумма чисел {a} и {b} равна {s}</h2>";
+        }
+        public FileResult GetFile()
+        {
+            string filePath = Server.MapPath("~/Files/Price.xlsx");
+            string fileType = "application/xlsx";
+            string fileName = "Price.xlsx";
+            return File(filePath, fileType, fileName);
         }
 
         public ActionResult About()
--- HotelBooking/Models/Order.cs
+++ HotelBooking/Models/Order.cs
@@ -9,7 +9,7 @@ namespace HotelBooking.Models
     {
         public int OrderId { get; set; }
         public string Person { get; set; }
-        public DateTime OrderDate { get; set; }
+        public DateTime Date { get; set; }
         public DateTime BookingBegin { get; set; }
         public DateTime BookingEnd { get; set; }
         public int RoomId { get; set; }
